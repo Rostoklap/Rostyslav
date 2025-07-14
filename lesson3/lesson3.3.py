3aa225b19b67b52d4f7cbadfa097a0f3de6d529e
Rostoklap <rostikapple97@gmail.com>
2025-07-14 22:35:08 +0300

    first commit

---
+++ lesson3/lesson3.3.py
@@ -0,0 +1,23 @@
+lst = [4, 7, 8, 10, 11]
+
+sep = (len(lst) + 1) // 2 # округление в большую сторону
+
+lst_new = [lst[:sep], lst[sep:]]
+
+print(lst_new)
+
+lst = [4]
+
+sep = (len(lst) + 1) // 2 # округление в большую сторону
+
+lst_new = [lst[:sep], lst[sep:]]
+
+print(lst_new)
+
+lst = []
+
+sep = (len(lst) + 1) // 2 # округление в большую сторону
+
+lst_new = [lst[:sep], lst[sep:]]
+
+print(lst_new)
