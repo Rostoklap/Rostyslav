68bb3fbb86a820de100ec53e29a53d821b0ca683
Rostoklap <rostikapple97@gmail.com>
2025-07-19 11:00:27 +0300

    first commit

--- lesson4/lesson4.1.py
+++ lesson4/lesson4.1.py
@@ -1,6 +1,10 @@
 lst = [0, 1, 4, 0, 5, 8]
 
 no_zero = [elem for elem in lst if elem != 0]
-zero = [0] * (len(lst) - len (no_zero))
-lst = no_zero + zero
+zero = len(lst) - len(no_zero)
+
+lst.clear()
+lst.extend(no_zero)
+lst.extend([0] * zero)
+
 print(lst)
