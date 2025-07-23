7a512974a5825591dc604e826b370f8750a0c5f3
Rostoklap <rostikapple97@gmail.com>
2025-07-23 14:41:11 +0300

    first commit

---
+++ lesson5/lesson5.2.py
@@ -0,0 +1,23 @@
+while True:
+    a = int(input("Введите первое число :"))
+    b = int(input("Введите второе число :"))
+    action = input("Введите действие ( + , - , * , /): ")
+
+    if action == "+":
+        print("Результат", a + b)
+    elif action == "-":
+        print("Результат", a - b)
+    elif action == "*":
+        print("Результат", a * b)
+    elif action == "/":
+        if b == 0:
+            print("0 нельзя!")
+        else:
+            print ("Результат", a / b)
+    else:
+        print("Неверное действие!")
+
+    cont = input("Продолжить? (y / n): ")
+    if cont.lower() != "y":
+        print("Завершение работы.")
+        break
